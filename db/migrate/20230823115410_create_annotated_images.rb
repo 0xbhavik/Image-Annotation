24e758e4c7b34b07615bb683bc9586490d22afdc
Inito <inito@Bhaviks-MBP.local>
2023-09-02 01:19:27 +0530

    removed all files

--- db/migrate/20230823115410_create_annotated_images.rb
+++
@@ -1,8 +0,0 @@
-class CreateAnnotatedImages < ActiveRecord::Migration[6.0]
-  def change
-    create_table :annotated_images do |t|
-      t.jsonb :annotations, null: false, default: '{}'
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20230823150054_add_name_to_annotated_image.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToAnnotatedImage < ActiveRecord::Migration[6.0]
-  def change
-    add_column :annotated_images, :name, :string
-  end
-end
